ccd01b79dce7dc8547a10613527a7ddbd3751ef8
Cole Caccamise <colecaccamise@gmail.com>
2024-05-01 23:49:32 -0400

    create basic router

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 // new entry point
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-	res.setHeader('Content-Type', 'text/html');
+	if (req.url == '/') {
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+		});
+		res.end('<h1>Home Page</h1>');
+	} else if (req.url == '/about') {
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+		});
+		res.end('<h1>About page</h1>');
+	} else {
+		res.writeHead(404, {
+			'Content-Type': 'text/html',
+		});
+		res.end('<h1>Page not found! Where are you trying to go...</h1>');
+	}
+
+	console.log(req.url);
+	console.log(req.method);
 
 	// res.statusCode = 404;
 
 	// res.write('<h1>Hello World</h1>');
-
-	res.writeHead(500, {
-		'Content-Type': 'text/html',
-	});
-	res.end();
 });
 
 server.listen(PORT, () => {
